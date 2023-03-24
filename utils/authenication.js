01a1041e428e247b414f5ab7279d26cabd9ad12b
maniyadg <maniyarasandhanasekaran@gmail.com>
2023-03-24 13:07:10 +0530

    cookiee added

--- utils/authenication.js
+++ utils/authenication.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken');
 exports.isAuth = async (req, res, next) => {
     const { cookies } = req;
 
-    if(req.cookies.accessToken){
-        let data = await jwt.verify(req.cookies.accessToken, process.env.SECRET_KEY);
+    if(cookies.accessToken){
+        let data = await jwt.verify(cookies.accessToken, process.env.SECRET_KEY);
         req.id = data._id;
         if(!req.id){
             return res.status(401).send({message: 'Not authorized.'})
