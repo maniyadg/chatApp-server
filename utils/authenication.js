a74d91d75168f162ef8770071dbce6e3d37d8fc8
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2023-09-28 11:05:10 +0530

    Update authenication.js

--- utils/authenication.js
+++ utils/authenication.js
@@ -4,7 +4,7 @@ exports.isAuth = async (req, res, next) => {
     const { cookies } = req;
 
     if(cookies.accessToken){
-        let data = await jwt.verify(cookies.accessToken, process.env.SECRET_KEY);
+        let data =  jwt.verify(cookies.accessToken, process.env.SECRET_KEY);
         req.id = data._id;
         if(!req.id){
             return res.status(401).send({message: 'Not authorized.'})
